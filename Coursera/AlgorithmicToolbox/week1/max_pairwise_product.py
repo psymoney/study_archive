38f12f948b71c80242e3f9895c5fe37ac6ff1f76
psymoney <78402001+psymoney@users.noreply.github.com>
2022-05-07 21:55:52 +0900

    fix max_pairwise_product assignment

--- Coursera/AlgorithmicToolbox/week1/max_pairwise_product.py
+++ Coursera/AlgorithmicToolbox/week1/max_pairwise_product.py
@@ -1,12 +1,29 @@
-def max_pairwise_product(numbers):
-    n = len(numbers)
-    max_product = 0
-    for first in range(n):
-        for second in range(first + 1, n):
-            max_product = max(max_product,
-                numbers[first] * numbers[second])
+# def max_pairwise_product(numbers):
+#     n = len(numbers)
+#     max_product = 0
+#     for first in range(n):
+#         for second in range(first + 1, n):
+#             max_product = max(max_product,
+#                 numbers[first] * numbers[second])
+#
+#     return max_product
 
-    return max_product
+
+def max_pairwise_product(numbers):
+    a = 0
+    b = 0
+
+    for e in numbers:
+        if e > a:
+            a = e
+
+    numbers.pop(numbers.index(a))
+
+    for e in numbers:
+        if e > b:
+            b = e
+
+    return a * b
 
 
 if __name__ == '__main__':
