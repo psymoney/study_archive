57d58d4c030ca15031d382cc8f395adadf1a0fed
psymonkey <sangykimiv@gmail.com>
2022-05-16 21:01:04 +0900

    deactivate test code method for submit

--- Coursera/AlgorithmicToolbox/week3/fractional_knapsack.py
+++ Coursera/AlgorithmicToolbox/week3/fractional_knapsack.py
@@ -46,7 +46,7 @@ def test_cases():
 
 
 if __name__ == "__main__":
-    test_cases()
+    # test_cases()
     data = list(map(int, sys.stdin.read().split()))
     n, capacity = data[0:2]
     values = data[2:(2 * n + 2):2]
