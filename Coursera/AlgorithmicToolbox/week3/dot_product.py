5f6b24da43d761036d4ff5e5a48505b5a7bc950b
psymoney <sangykimiv@gmail.com>
2022-05-18 08:56:44 +0900

    add problem introductions

--- Coursera/AlgorithmicToolbox/week3/dot_product.py
+++ Coursera/AlgorithmicToolbox/week3/dot_product.py
@@ -1,7 +1,14 @@
 """
-# Maximum Advertisement Revenue
+## Maximum Advertisement Revenue
 
-## Problem Description
+# Problem Introduction
+You have 𝑛 ads to place on a popular Internet page. For each ad, you know how
+much is the advertiser willing to pay for one click on this ad. You have set up 𝑛
+slots on your page and estimated the expected number of clicks per day for each
+slot. Now, your goal is to distribute the ads among the slots to maximize the
+total revenue.
+
+# Problem Description
 Task.   Given two sequences 𝑎1, 𝑎2, . . . , 𝑎𝑛 (𝑎𝑖 is the profit per click of the 𝑖-th ad) and 𝑏1, 𝑏2, . . . , 𝑏𝑛 (𝑏𝑖 is
         the average number of clicks per day of the 𝑖-th slot), we need to partition them into 𝑛 pairs (𝑎𝑖, 𝑏𝑗)
         such that the sum of their products is maximized.
