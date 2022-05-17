54c01c715a87a6d22f6691bccf21db0cf480a0bf
psymoney <sangykimiv@gmail.com>
2022-05-17 16:47:57 +0900

    solve week3-4 assignment

---
+++ Coursera/AlgorithmicToolbox/week3/dot_product.py
@@ -0,0 +1,44 @@
+"""
+# Maximum Advertisement Revenue
+
+## Problem Description
+Task.   Given two sequences 𝑎1, 𝑎2, . . . , 𝑎𝑛 (𝑎𝑖 is the profit per click of the 𝑖-th ad) and 𝑏1, 𝑏2, . . . , 𝑏𝑛 (𝑏𝑖 is
+        the average number of clicks per day of the 𝑖-th slot), we need to partition them into 𝑛 pairs (𝑎𝑖, 𝑏𝑗)
+        such that the sum of their products is maximized.
+Input Format.   The first line contains an integer 𝑛, the second one contains a sequence of integers
+        𝑎1, 𝑎2, . . . , 𝑎𝑛, the third one contains a sequence of integers 𝑏1, 𝑏2, . . . , 𝑏𝑛.
+Constraints.    1 ≤ 𝑛 ≤ 10^3; −10^5 ≤ 𝑎𝑖, 𝑏𝑖 ≤ 10^5 for all 1 ≤ 𝑖 ≤ 𝑛.
+Output Format.  Output the maximum value of Σ𝑛𝑖=1𝑎𝑖𝑐𝑖, where 𝑐1, 𝑐2, . . . , 𝑐𝑛 is a permutation of 𝑏1, 𝑏2, . . . , 𝑏𝑛.
+"""
+import sys
+
+
+def max_dot_product(a, b):
+    revenue = 0
+    a.sort(reverse=True)
+    b.sort(reverse=True)
+    for i in range(len(a)):
+        revenue += a[i] * b[i]
+    return revenue
+
+
+def test_cases():
+    if max_dot_product([23], [39]) == 897:
+        print('first case success')
+    else:
+        print('first case failed')
+    if max_dot_product([1, 3, -5],[-2, 4, 1]) == 23:
+        print('second case success')
+    else:
+        print('second case failed')
+
+
+if __name__ == '__main__':
+    # test_cases()
+    input = sys.stdin.read()
+    data = list(map(int, input.split()))
+    n = data[0]
+    a = data[1:(n + 1)]
+    b = data[(n + 1):]
+    print(max_dot_product(a, b))
+    
