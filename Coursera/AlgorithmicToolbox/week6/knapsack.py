4036f5044047458325043d731c048f3038597556
psymoney <sangykimiv@gmail.com>
2022-06-08 11:53:37 +0900

    add problem descriptions

--- Coursera/AlgorithmicToolbox/week6/knapsack.py
+++ Coursera/AlgorithmicToolbox/week6/knapsack.py
@@ -1,4 +1,42 @@
-# Uses python3
+"""
+## Maximum Amount of Gold
+
+# Problem Introduction
+    You are given a set of bars of gold and your goal is to take as much gold as possible into
+    your bag. There is just one copy of each bar and for each bar you can either take it or not
+    (hence you cannot take a fraction of a bar).
+
+# Problem Description
+    Task.           Given 𝑛 gold bars, find the maximum weight of gold that fits into a bag of capacity 𝑊.
+    Input Format.   The first line of the input contains the capacity 𝑊 of a knapsack and the number 𝑛 of bars
+                    of gold. The next line contains 𝑛 integers 𝑤0,𝑤1, . . . ,𝑤𝑛−1 defining the weights of the bars of gold.
+    Constraints.    1 ≤ 𝑊 ≤ 104; 1 ≤ 𝑛 ≤ 300; 0 ≤ 𝑤0, . . . ,𝑤𝑛−1 ≤ 105.
+    Output Format.  Output the maximum weight of gold that fits into a knapsack of capacity 𝑊.
+
+# Sample 1.
+    Input:
+        10 3
+        1 4 8
+    Output:
+        9
+
+# Starter Files
+    Starter files contain an implementation of the following greedy strategy: scan the list of given bars of gold
+    and add the current bar if it fits into the current capacity (note that, in this problem, all the items have the
+    same value per unit of weight, for a simple reason: they are all made of gold). As you already know from the
+    lectures, such a greedy move is not safe. You may want to additionally submit a starter file as a solution to
+    the grading system to ensure that this greedy algorithm indeed might produce a non-optimal result.
+
+# Solution
+    A detailed solution (with Python code) for this challenge is covered in the companion MOOCBook. We
+    strongly encourage you to do your best to solve the challenge yourself before looking into the book! There
+    are at least three good reasons for this.
+        • By solving this challenge, you practice solving algorithmic problems similar to those given at technical
+        interviews.
+        • The satisfaction and self confidence that you get when passing the grader is priceless =)
+        • Even if you fail to pass the grader yourself, the time will not be lost as you will better understand the
+        solution from the book and better appreciate the beauty of the underlying ideas.
+"""
 import sys
 
 def optimal_weight(W, w):
--- Coursera/AlgorithmicToolbox/week6/placing_parentheses.py
+++ Coursera/AlgorithmicToolbox/week6/placing_parentheses.py
@@ -1,4 +1,34 @@
-# Uses python3
+"""
+## Maximum Value of an Arithmetic Expression
+
+# Problem Introduction
+    In this problem, your goal is to add parentheses to a given arithmetic
+    expression to maximize its value. max(5−8+7×4−8+9) =?
+
+# Problem Description
+    Task.           Find the maximum value of an arithmetic expression by specifying the order of applying its arithmetic
+                    operations using additional parentheses.
+    Input Format.   The only line of the input contains a string 𝑠 of length 2𝑛 + 1 for some 𝑛, with symbols
+                    𝑠0, 𝑠1, . . . , 𝑠2𝑛. Each symbol at an even position of 𝑠 is a digit (that is, an integer from 0 to 9) while
+                    each symbol at an odd position is one of three operations from {+,-,*}.
+    Constraints.    0 ≤ 𝑛 ≤ 14 (hence the string contains at most 29 symbols).
+    Output Format.  Output the maximum possible value of the given arithmetic expression among different
+                    orders of applying arithmetic operations.
+
+# Sample 1.
+    Input:
+        1+5
+    Output:
+        6
+
+# Sample 2.
+    Input:
+        5-8+7*4-8+9
+    Output:
+        200
+        200 = (5 − ((8 + 7) × (4 − (8 + 9))))
+
+"""
 def evalt(a, b, op):
     if op == '+':
         return a + b
